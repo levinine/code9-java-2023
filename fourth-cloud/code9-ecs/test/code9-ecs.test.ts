e60d40dc63365ad2f590264574cf26a385737efd
Dusan Maric <d.maric@tm-pro.eu>
2023-04-12 09:15:34 +0200

    Add ecs example

---
+++ fourth-cloud/code9-ecs/test/code9-ecs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Code9Ecs from '../lib/code9-ecs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/code9-ecs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Code9Ecs.Code9EcsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
